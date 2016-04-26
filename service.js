116b6be55404d49a23390e424ddcc429d9a4799b
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-04-26 08:59:30 +0200

    Initial commit

---
+++ service.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var Seneca = require('seneca')
+var Mesh = require('seneca-mesh')
+var envs = process.env
+var Exchange = require('./lib/exchange')
+
+var options = {
+  seneca: {
+    tag: envs.TASKS_COLLECTOR_EXCHANGE_TAG || 'tasks-collector-exchange'
+  },
+  mesh: {
+    auto: true
+  },
+  exchange: {
+    url: envs.TASKS_COLLECTOR_EXCHANGE_URL || 'http://www.exchange.no'
+  },
+  isolated: {
+    host: envs.TASKS_COLLECTOR_EXCHANGE_HOST || 'localhost',
+    port: envs.TASKS_COLLECTOR_EXCHANGE_PORT || '8000'
+  }
+}
+var Service = Seneca(options.seneca)
+
+if (envs.TASKS_COLLECTOR_EXCHANGE_ISOLATED) {
+  Service.listen(options.isolated)
+}
+else {
+  Service.use(Mesh, options.mesh)
+}
+
+Service.use(Exchange, options.exchange)
