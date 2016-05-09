e6a925b0a93fa22624141b21bd395a173fd76718
maccyber <jonas.enge@gmail.com>
2016-05-09 15:36:08 +0200

    Added support for impersonation

--- config/index.js
+++ config/index.js
@@ -6,6 +6,7 @@ var config = {
   url: envs.EWS_URL || 'https://epost.vfk.no/ews/Exchange.asmx',
   username: envs.EWS_USERNAME || 'domain\\username', // Must have double slash between domain and user
   password: envs.EWS_PASSWORD || 'password',
+  domain: envs.EWS_DOMAIN || '@skole.t-fk.no',
   tasksUrl: envs.EWS_TASKS_URL || 'https://epost.vfk.no/owa/#path=/tasks',
   tasks: envs.EWS_TASKS || 20 // number of uncompleted tasks to get
 }
