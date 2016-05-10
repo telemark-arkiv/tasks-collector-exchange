004246afa89868e98636cb627cffb0878d346465
Jonas Enge <jonas.enge@t-fk.no>
2016-05-10 10:40:46 +0200

    Changed envs

--- config/index.js
+++ config/index.js
@@ -3,12 +3,12 @@
 var envs = process.env
 
 var config = {
-  url: envs.EWS_URL || 'https://epost.vfk.no/ews/Exchange.asmx',
-  username: envs.EWS_USERNAME || 'domain\\username', // Must have double slash between domain and user
-  password: envs.EWS_PASSWORD || 'password',
-  domain: envs.EWS_DOMAIN || '@skole.t-fk.no',
-  tasksUrl: envs.EWS_TASKS_URL || 'https://epost.vfk.no/owa/#path=/tasks',
-  tasks: envs.EWS_TASKS || 20 // number of uncompleted tasks to get
+  url: envs.EWS_TASKS_URL || 'https://epost.vfk.no/ews/Exchange.asmx',
+  username: envs.EWS_TASKS_USERNAME || 'domain\\username', // Must have double slash between domain and user
+  password: envs.EWS_TASKS_PASSWORD || 'password',
+  domain: envs.EWS_TASKS_DOMAIN || '@skole.t-fk.no',
+  tasksUrl: envs.EWS_TASKS_OWA_URL || 'https://epost.vfk.no/owa/#path=/tasks',
+  tasks: envs.EWS_TASKS_NUMBER || 20 // number of uncompleted tasks to get
 }
 
 module.exports = config
