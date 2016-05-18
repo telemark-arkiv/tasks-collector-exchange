21b42a12cc965ff7c6e84d0a3c4d57a9e22cf121
Jonas Enge <jonas.enge@t-fk.no>
2016-05-18 11:33:42 +0200

    Removes @ from domain

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ var config = {
   url: envs.EWS_TASKS_URL || 'https://epost.vfk.no/ews/Exchange.asmx',
   username: envs.EWS_TASKS_USERNAME || 'domain\\username', // Must have double slash between domain and user
   password: envs.EWS_TASKS_PASSWORD || 'password',
-  domain: envs.EWS_TASKS_DOMAIN || '@skole.t-fk.no',
+  domain: envs.EWS_TASKS_DOMAIN || 'skole.t-fk.no',
   tasksUrl: envs.EWS_TASKS_OWA_URL || 'https://epost.vfk.no/owa/#path=/tasks',
   tasksLimit: envs.EWS_TASKS_LIMIT || 20 // number of uncompleted tasks to get
 }
